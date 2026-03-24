2f4ec393c13ef3120f79b8d475f692f136a271e5
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2026-03-24 21:32:49 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  turbopack: {
-    root: ".",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
